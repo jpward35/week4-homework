d8e787477013bfd754510d76b132cda16a2ad1cd
James P. Ward <jward2015@kellogg.northwestern.edu>
2014-02-05 12:10:48 -0600

    Week 4 HW
    Updated HW

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,2 +1,10 @@
   class PagesController < ApplicationController
+
+    def index
+    end
+
+    def show
+    end
+
+
   end
--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,11 @@ Week4hw::Application.routes.draw do
   get "/" => "pages#home"
 
   # HINT: Add more routes here...
+    get "/weather/search" => "weather#search"
+    get"/weather/conditions" => "weather#conditions"
 
+    get"/index" => "pages#index"
+    get"/show" => "pages#show"
 
 
 end
